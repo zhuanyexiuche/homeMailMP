c9e038a03e78382412cfc4a2969eacd6235acdf1
4thcalabash <751188973@qq.com>
2018-03-09 08:51:54 +0800

    补充现有逻辑

--- pages/treeHole/treeHole.js
+++ pages/treeHole/treeHole.js
@@ -6,14 +6,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-    treeHoleList: [{ id: "0", content: "我想放假啊啊啊，不想开学啊啊啊啊" }, { id: "1", content: "决定攒钱买一个Chanel包包" }, { id: "2", content: "想看一场wuli何美男的演唱会" }, { id: "3", content: "一定要再去一次厦门，去鼓浪屿看日出" }, { id: "4", content: "程序员需要听从颈椎康复指南手册的指导" }, { id: "5", content: "妈耶我编不出来了啊哭泣" }, { id: "6", content: "" }, { id: "7", content: "" }, { id: "8", content: "" }]
+    treeHoleList: null/*[{ id: "0", content: "我想放假啊啊啊，不想开学啊啊啊啊" }, { id: "1", content: "决定攒钱买一个Chanel包包" }, { id: "2", content: "想看一场wuli何美男的演唱会" }, { id: "3", content: "一定要再去一次厦门，去鼓浪屿看日出" }, { id: "4", content: "程序员需要听从颈椎康复指南手册的指导" }, { id: "5", content: "妈耶我编不出来了啊哭泣" }, { id: "6", content: "" }, { id: "7", content: "" }, { id: "8", content: "" }]*/
   },
-
+  refresh:function(succ){
+    let that = this;
+    wx.request({
+      url: app.globalData.serverUrl+'/readSecret',
+      data:{
+        brief:true
+      },
+      success:function(res){
+        that.setData({
+          treeHoleList:res.data
+        });
+        console.log(res);
+        if (succ)succ();
+      },
+      fail:function(reason){
+        console.log(reason);
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.refresh();
   },
 
   /**
@@ -27,7 +45,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this.refresh();
   },
 
   /**
@@ -48,7 +66,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    this.refresh(wx.stopPullDownRefresh());
   },
 
   /**
